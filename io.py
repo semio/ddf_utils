5bd17fb95931b68515826ca11900f7bbb9b773ba
Semio Zheng <prairy.long@gmail.com>
2016-04-14 08:47:07 +0800

    first commit

---
+++ io.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""io functions for ddf files"""
+
+import os
+# import pandas as pd
+
+
+def to_csv(df, out_dir, ftype, concept, by=None, **kwargs):
+    """save a ddf dataframe to csv file.
+    the file path of csv will be out_dir/ddf--$ftype--$concept--$by.csv
+    """
+
+    if not by:
+        path = os.path.join(out_dir, 'ddf--'+ftype+'--'+concept+'.csv')
+    else:
+        if isinstance(by, list):
+            filename = 'dff' + '--'.join([ftype, concept]) + '--'.join(by) + '.csv'
+        else:
+            filename = 'dff' + '--'.join([ftype, concept, by]) + '.csv'
+
+        path = os.path.join(out_dir, filename)
+
+    df.to_csv(path, **kwargs)
---
+++ str.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""string functions for ddf files"""
+
+import re
+
+
+# TODO: make the sub parameter just a list of string, not expr
+# using re.escape()
+def to_concept_id(s, sub='[/ -\.\*";]+', sep='_'):
+    '''convert a string to lowercase alphanumeric + underscore id for concepts'''
+    if s is np.nan:
+        return s
+
+    s1 = re.sub(sub, sep, s.strip())
+    s1 = s1.replace('\n', '')
+
+    # remove the first/last underscore
+    if s1[-1] == sep:
+        s1 = s1[:-1]
+    if s1[0] == sep:
+        s1 = s1[1:]
+
+    return s1.lower()
