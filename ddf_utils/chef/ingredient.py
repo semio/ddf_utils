af8f9d6be730bd11be9ef51fb788d337accfeda3
semio <prairy.long@gmail.com>
2016-11-22 15:12:35 +0800

    new: ddf_reader base on datapackage
    also updated related chef functions

--- ddf_utils/chef/ingredient.py
+++ ddf_utils/chef/ingredient.py
@@ -7,6 +7,7 @@ import os
 import logging
 
 from .. import config
+from ..ddf_reader import DDF
 
 
 class Ingredient(object):
@@ -16,15 +17,14 @@ class Ingredient(object):
     """
     def __init__(self, ingred_id, ddf_id, key, values, row_filter=None, data=None):
         self.ingred_id = ingred_id
-        self.ddf_id = ddf_id
         self.key = key
         self.values = values
         self.row_filter = row_filter
+        if ddf_id:
+            self.ddf = DDF(ddf_id)
+        else:
+            self.ddf = None
         self.data = data
-        self._index = None
-        # last_update is a dataframe contains last update
-        # time for each file in this ingredient.
-        self.last_update = None
 
     @classmethod
     def from_dict(cls, data):
@@ -41,13 +41,7 @@ class Ingredient(object):
 
     @property
     def ddf_path(self):
-        return _get_ddf_path(self.ddf_id)
-
-    @property
-    def index(self):
-        if self._index is None:
-            self._index = _get_index(self.ddf_id)
-        return self._index
+        return self.ddf.dataset_path
 
     @property
     def dtype(self):
@@ -67,68 +61,71 @@ class Ingredient(object):
     def __repr__(self):
         lines = list()
         lines.append('Ingredient: ' + self.ingred_id)
-        lines.append('dataset: '+self.ddf_id)
-        lines.append('key: '+str(self.key))
-        lines.append('values: '+str(self.values))
-        if self.row_filter:
-            lines.append('row_filter: Yes')
+        lines.append('Data type: '+self.dtype)
+        if self.ddf:
+            lines.append('Dataset: '+self.ddf.ddf_id)
         else:
-            lines.append('row_filter: No')
+            lines.append('Dataset: None')
+        lines.append('Key: '+str(self.key))
+        lines.append('Values: '+str(self.values))
+        if self.row_filter:
+            lines.append('Row_filter: Yes')
+        else:
+            lines.append('Row_filter: No')
 
         return '\n'.join(lines)
 
-    def key_to_list(self):
-        if self.dtype == "datapoints":
-            return self.key.split(',')
+    def _get_data_datapoint(self, copy):
+        data = dict()
+        keys = self.key.split(',')
+        if self.values == '*':  # get all datapoints for the key
+            values = []
+            for k, v in self.ddf.get_datapoint_files().items():
+                for pkeys in v.keys():
+                    if set(keys) == set(pkeys):
+                        values.append(k)
         else:
-            assert isinstance(self.key, str)
-            return [self.key]
-
-    def filter_index_key_value(self):
-        """filter index file by key and value"""
-        index = self.index
-        key = self.key
-
-        if isinstance(self.values, list):
             values = self.values
-            if isinstance(self.key, list):
-                filtered = index[(index["key"].isin(key)) & (index['value'].isin(values))]
-            else:
-                filtered = index[(index["key"] == key) & (index['value'].isin(values))]
-        else:  # assuming value = "*"
-            if isinstance(self.key, list):
-                filtered = index[index["key"].isin(key)]
-            else:
-                filtered = index[index["key"] == key]
 
-        if len(filtered) == 0:
-            raise ValueError(self.ingred_id +
-                             ": No data found for the key/value pair! check your ingredient.")
+        if not values or len(values) == 0:
+            raise ValueError('no datapoint found for the ingredient: ' + self.ingred_id)
 
-        return filtered
+        for v in values:
+            data[v] = self.ddf.get_datapoint_df(v, keys)
+        return data
 
-    def filter_index_file_name(self):
-        """
-        filter index by keywords in the file names.
-        This method should only called for entities related tasks.
-        """
-        index = self.index
-        key = self.key
-
-        if isinstance(key, list):
-            bools = []
-            for k in key:
-                bools.append(index['file'].str.contains(k))
-
-            ser_0 = bools[0]
-            for ser in bools[1:]:
-                ser_0 = ser_0 | ser
-
-            full_list = index[ser_0]
+    def _get_data_entities(self, copy):
+        # TODO: values should always be '*'?
+        if copy:
+            ent = self.ddf.get_entities(dtype=str)
         else:
-            full_list = index[index['file'].str.contains(key)]
+            ent = self.ddf.get_entities()
+        if self.key == '*':
+            return ent
+        else:
+            conc = self.ddf.get_concepts()
 
-        return full_list[full_list['file'].str.contains(self.dtype)]
+            values = []
+
+            for v in self.key:
+                if conc.ix[v, 'concept_type'] == 'entity_domain':
+                    if 'domain' in conc.columns and len(conc[conc['domain'] == v]) > 0:
+                        [values.append(i) for i in conc[conc['domain'] == v].index]
+                    else:
+                        values.append(v)
+                else:
+                    values.append(v)
+
+            return dict((k, ent[k]) for k in values)
+
+    def _get_data_concepts(self, copy):
+        conc = {
+            'concepts': self.ddf.get_concepts()
+        }
+        if self.values == '*':
+            return conc
+        else:
+            return conc[values]
 
     def filter_row(self, df):
         """return the rows selected by self.row_filter."""
@@ -142,45 +139,27 @@ class Ingredient(object):
 
         return df
 
-    def get_data(self):
-
-        # if data is not empty, then it will just return the data
-        # when get_data() is called.
-        if self.data is not None:
-            return self.data
-
+    def get_data(self, copy=False):
         funcs = {
-            # all functions will return a dictionary of dataframes.
-            'concepts': self._get_data_concept,
-            'entities': self._get_data_entity,
-            'datapoints': self._get_data_datapoint
+            'datapoints': self._get_data_datapoint,
+            'entities': self._get_data_entities,
+            'concepts': self._get_data_concepts
         }
-
-        self.data = funcs[self.dtype]()
+        if self.data is None:
+            data = funcs[self.dtype](copy)
+            for k, v in data.items():
+                if self.row_filter:
+                    index_cols = data[k].index.names
+                    # data[k] = self.filter_row(data[k].reset_index()).set_index(index_cols)
+                    data[k] = self.filter_row(data[k].reset_index())
+                else:
+                    data[k] = data[k].reset_index()
+            self.data = data
         return self.data
-
-    def get_data_str(self):
-        """
-        this function will read the related data as strings for all columns,
-        using the filename as key in the result
-        """
-        if self.dtype == 'entities':
-            filtered = self.filter_index_file_name()
-        else:
-            filtered = self.filter_index_key_value()
-
-        ddf_path = self.ddf_path
-
-        res = []
-
-        for f in set(filtered['file'].values):
-            df = pd.read_csv(os.path.join(ddf_path, f), dtype=str)
-            res.append([f, df])
-
-        return dict(res)
 
     def reset_data(self):
         self.data = None
+        return
 
     def copy_data(self):
         """this function makes copy of self.data.
@@ -191,105 +170,10 @@ class Ingredient(object):
         # but I just call it explicitly here.
         return dict((k, v.copy(deep=True)) for k, v in self.data.items())
 
-    def get_last_update(self):
-        if self.last_update is not None:
-            return self.last_update
-
-        index = self.index
-
-        for f in index['file'].drop_duplicates().values:
-            path = os.path.join(config.SEARCH_PATH, self.ddf_id, f)
-            mtime = os.path.getmtime(path)
-
-            index.loc[index['file'] == f, 'last_update'] = mtime
-
-        self.last_update = index[['file', 'last_update']].drop_duplicates()
-        return self.last_update
-
-    def _get_data_datapoint(self):
-        ddf_path = self.ddf_path
-
-        filtered = self.filter_index_key_value()
-
-        res = []
-
-        # map all index columns to string type
-        keys = self.key_to_list()
-        dtype_conf = dict([(x, str) for x in keys])
-
-        for i, row in filtered.iterrows():
-            df = pd.read_csv(os.path.join(ddf_path, row['file']), dtype=dtype_conf)
-            df = self.filter_row(df)
-
-            res.append([row['value'], df])
-
-        return dict(res)
-
-    def _get_data_concept(self):
-        ddf_path = self.ddf_path
-
-        filtered = self.filter_index_key_value()
-
-        res = []
-        for f in set(filtered['file'].values):
-            if 'continuous' in f:
-                key = 'continuous'
-            elif 'discrete' in f:
-                key = 'discrete'
-            else:
-                key = 'concept'
-
-            df = pd.read_csv(os.path.join(ddf_path, f))
-
-            if isinstance(self.values, list):
-                df = df[self.values]
-
-            df = self.filter_row(df)
-            res.append([key, df])
-        return dict(res)
-
-    def _get_data_entity(self):
-        ddf_path = self.ddf_path
-
-        filtered = self.filter_index_file_name()
-
-        res = []
-
-        for f in set(filtered['file'].values):
-            entity = f[:-4].split('--')[-1]
-
-            # if re.match('ddf--entities--.*--.*.csv', f):
-                # domain = re.match('ddf--entities--(.*)--.*.csv', f).groups()[0]
-
-            df = pd.read_csv(os.path.join(ddf_path, f), dtype=str)
-
-            if isinstance(self.values, list):
-                df = df[self.values]
-            df = self.filter_row(df)
-            res.append([entity, df])
-
-        return dict(res)
-
-
-# helper functions for Ingredient
-def _get_ddf_path(ddf_id):
-    path = os.path.join(config.SEARCH_PATH, ddf_id)
-    if os.path.exists(path):
-        return path
-    else:
-        raise ValueError('data set not found: {}'.format(ddf_id))
-
-
-def _get_index(ddf_id):
-    """
-    return the index file of ddf_id.
-    if the file don't exists, create one
-    """
-    ddf_path = _get_ddf_path(ddf_id)
-    index_path = os.path.join(ddf_path, 'ddf--index.csv')
-
-    if os.path.exists(index_path):
-        return pd.read_csv(index_path)
-    else:
-        from .. index import create_index_file
-        return create_index_file(ddf_path)
+    def key_to_list(self):
+        """helper function: make a list that contains primaryKey of this ingredient"""
+        if self.dtype == "datapoints":
+            return self.key.split(',')
+        else:
+            assert isinstance(self.key, str)
+            return [self.key]
