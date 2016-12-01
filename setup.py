246b8e854302dd7c909779b4205a9a18797cef4d
semio <prairy.long@gmail.com>
2016-12-01 15:17:09 +0800

    version 0.2.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 import os
 from setuptools import setup, find_packages
 
-version = "0.2.4-dev"
+version = "0.2.5-dev"
 
 if sys.argv[-1] == 'tag':
     os.system("git tag -a %s -m 'version %s'" % (version, version))
