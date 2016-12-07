227b28c03f271fb03425f414129bde3cadb639e5
semio <prairy.long@gmail.com>
2016-12-07 16:30:25 +0800

    skip same procedure from include

--- ddf_utils/chef/cook.py
+++ ddf_utils/chef/cook.py
@@ -111,7 +111,7 @@ def build_recipe(recipe_file, to_disk=False):
                         # recipe['cooking'][p] = [*rcp['cooking'][p], *recipe['cooking'][p]]  # not supportted by Python < 3.5
                         new_procs = []
                         [new_procs.append(proc) for proc in rcp['cooking'][p]]
-                        [new_procs.append(proc) for proc in recipe['cooking'][p]]
+                        [new_procs.append(proc) for proc in recipe['cooking'][p] if proc not in new_procs]
                         recipe['cooking'][p] = new_procs
                     else:
                         recipe['cooking'][p] = rcp['cooking'][p]
