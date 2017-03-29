2205d0abc039577d4eb96bc4f825161b25028137
semio <prairy.long@gmail.com>
2017-03-29 11:13:24 +0800

    fix test path

--- tests/common.py
+++ tests/common.py
@@ -3,5 +3,4 @@
 import os
 import ddf_utils
 
-modulepath = os.path.dirname(ddf_utils.__file__)
-ddf_utils.config.DDF_SEARCH_PATH = os.path.join(modulepath, '../tests/datasets')
+ddf_utils.config.DDF_SEARCH_PATH = './datasets'
