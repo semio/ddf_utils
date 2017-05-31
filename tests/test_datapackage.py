d471adb2e287a0c372773d713fcdc8661cd10e9e
semio <prairy.long@gmail.com>
2017-05-31 10:45:05 +0800

    update some tests

---
+++ tests/test_datapackage.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+
+def test_create_datapackage():
+    from ddf_utils.datapackage import get_datapackage
+    from ddf_utils.model.datapackage import Datapackage
+
+    dataset_path = os.path.join(os.path.dirname(__file__),
+                                'datasets/ddf--gapminder--dummy_companies')
+
+    dp1 = get_datapackage(dataset_path, use_existing=True)
+    assert dp1['license'] == 'foo'
+
+    dp2 = get_datapackage(dataset_path, use_existing=False)
+    assert 'license' not in dp2.keys()
+
+    dp_ = Datapackage(dataset_path)
+    dp_.generate_ddfschema()
+    assert 'ddfSchema' in dp_.datapackage.keys()
+
+
+def test_dataset():
+    from ddf_utils.model.ddf import Dataset
+
+    dataset_path = os.path.join(os.path.dirname(__file__),
+                                'datasets/ddf--gapminder--dummy_companies')
+
+    ds = Dataset.from_ddfcsv(dataset_path)
+
+    conc = ds.concepts
+    ent = ds.entities
+    dps = ds.datapoints
+
+    ent_foundation = ds.get_entity('foundation')
+    assert 'is--foundation' in ent_foundation.columns
