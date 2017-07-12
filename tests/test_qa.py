f8e1c7bd61746960da7fb761bee9669f198aba02
semio <prairy.long@gmail.com>
2017-07-12 10:09:30 +0800

    update test for qa module

--- tests/test_qa.py
+++ tests/test_qa.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import common
-from ddf_utils import DDF
+import os
+from ddf_utils.model.ddf import Dataset
 import numpy as np
 from numpy.testing import assert_array_equal
+
+wd = os.path.dirname(__file__)
 
 
 def test_compare_func():
     from ddf_utils.qa import compare_with_func
-    d1 = DDF('ddf--bp--energy')
-    d2 = DDF('ddf--cme')
+    d1 = Dataset.from_ddfcsv(os.path.join(wd, 'datasets/ddf--bp--energy'))
+    d2 = Dataset.from_ddfcsv(os.path.join(wd, 'datasets/ddf--cme'))
 
     res1 = compare_with_func(d1, d1)
     res2 = compare_with_func(d1, d2)
