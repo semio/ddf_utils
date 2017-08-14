03f8a65239af0bcc6f52679eb96ca719d69115ac
semio <prairy.long@gmail.com>
2017-08-14 11:42:54 +0800

    fix versions icon

--- setup.py
+++ setup.py
@@ -48,8 +48,8 @@ setup(
     keywords=['etl', 'ddf', 'datasets', 'recipe'],
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
+        # "Programming Language :: Python :: 3.3",
+        # "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Topic :: Utilities"
