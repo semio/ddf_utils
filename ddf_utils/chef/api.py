4c089aa2fb668be0906cc0a3b445b57f1998ce24
semio <prairy.long@gmail.com>
2017-11-13 00:25:59 +0800

    new api function

--- ddf_utils/chef/api.py
+++ ddf_utils/chef/api.py
@@ -2,3 +2,16 @@
 
 from ddf_utils.chef.cook import Chef
 
+
+def run_recipe(fn, out_dir):
+    """run the recipe file and serve result"""
+    from ddf_utils.io import cleanup
+    import os
+    if os.path.exists(out_dir):
+        cleanup(out_dir)
+    else:
+        os.mkdir(out_dir)
+
+    chef = Chef.from_recipe(fn)
+    chef.run(serve=True, out_dir=out_dir)
+    return
