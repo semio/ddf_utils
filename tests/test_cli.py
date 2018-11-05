9196877cef83dcb3382be7f24aa66c91635eafe1
semio <prairy.long@gmail.com>
2018-11-05 16:22:34 +0800

    more logs

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -62,6 +62,10 @@ def test_ddf_cli():
     base_path = os.path.dirname(__file__)
 
     runner = CliRunner()
+
+    result = runner.invoke(ddf)
+    click.echo(result.output)
+
     result = runner.invoke(ddf, args=['build_recipe',
                                       os.path.join(base_path, 'chef/recipes/test_flatten.yml')])
     click.echo(result.output)
