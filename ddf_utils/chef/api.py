33767b2eb1eab8f509073496a6150d685e2d40f7
semio <prairy.long@gmail.com>
2019-05-02 10:17:11 +0800

    fix test case

--- ddf_utils/chef/api.py
+++ ddf_utils/chef/api.py
@@ -18,8 +18,6 @@ def run_recipe(fn, ddf_dir, out_dir):
     else:
         os.mkdir(out_dir)
 
-    chef = Chef.from_recipe(fn)
-    if ddf_dir is not None:
-        chef.add_config(ddf_dir=ddf_dir)
+    chef = Chef.from_recipe(fn, ddf_dir=ddf_dir)
     chef.run(serve=True, outpath=out_dir)
     return
