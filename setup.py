b6a14617ab89bcb901f9fc5d47d53bd4d0291d25
semio <semio@posteo.net>
2020-01-30 20:37:04 +0800

    add 3.8 CI

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
         # "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Utilities"
     ]
 )
