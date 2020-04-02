ace71fa409a2f92d67169822f26812b8c9745737
semio <semio@posteo.net>
2020-04-02 23:54:17 +0800

    version

--- ddf_utils/__init__.py
+++ ddf_utils/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '1.0.8'
+__version__ = '1.0.9dev0'
 
 from . import (str, cli, i18n, io, patch, qa, transformer, factory)
 from ddf_utils.model.package import DataPackage
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 
 from setuptools import find_packages, setup
 
-version = "1.0.8"
+version = "1.0.9dev0"
 
 if sys.argv[-1] == 'tag':
     os.system("git tag -a %s -m 'version %s'" % (version, version))
