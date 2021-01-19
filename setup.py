e3489f4bd54d5a62628a4e83a7cb956b6daca193
semio <semio@posteo.net>
2021-01-19 23:36:04 +0800

    version 1.0.12

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 
 from setuptools import find_packages, setup
 
-version = "1.0.11"
+version = "1.0.12"
 
 if sys.argv[-1] == 'tag':
     os.system("git tag -a %s -m 'version %s'" % (version, version))
